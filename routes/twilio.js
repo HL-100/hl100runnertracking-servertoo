b3c9016f1212df05268d75a42c3c5a5782870c99
Brandon Johnson <bmj1985@gmail.com>
2018-03-30 16:13:45 -0600

    updated twilio to parse texts more betterer

--- routes/twilio.js
+++ routes/twilio.js
@@ -1,34 +1,46 @@
-const express = require("express");
-const queries = require("../queries/runnerqueries");
-const MessagingResponse = require("twilio").twiml.MessagingResponse;
+const express = require('express');
+const queries = require('../queries/runnerqueries');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
 const router = express.Router();
 
-function stringToBoolean(string) {
-  let newBoolean;
-  if (string === "yes") {
-    return (newBoolean = true);
-  } else if (string === "no") {
-    return (newBoolean = false);
-  } else {
-    return (newBoolean = null);
+function pacerParser(string) {
+  if (string === 'piy') {
+    return 'Yes';
+  } else if (string === 'poy') {
+    return 'Yes';
+  } else if (string === 'pin') {
+    return 'No';
+  } else if (string === 'pon') {
+    return 'No';
   }
 }
 
-router.post("/sms", (request, response) => {
-  const twiml = new MessagingResponse();
-  twiml.message("Message received!");
-  let msg = request.body.Body.replace(/,/g, "").split(" ");
-  let as = msg[0];
-  let id = msg[1];
-  let updateBody = {
-    [`${as}In`]: msg[2],
-    [`${as}Out`]: msg[3],
-    [`${as}PacerIn`]: stringToBoolean(msg[4]),
-    [`${as}PacerOut`]: stringToBoolean(msg[5])
+function formatBody(string) {
+  const newMsg = string.replace(/,/g, '').split(' ');
+  if (newMsg[4] != undefined) {
+    const body = {
+      [`${newMsg[0]}In`]: newMsg[2],
+      [`${newMsg[0]}Out`]: newMsg[3],
+      [`${newMsg[0]}PacerIn`]: pacerParser(newMsg[4]),
+      [`${newMsg[0]}PacerOut`]: pacerParser(newMsg[5]),
+    };
+    return body;
+  }
+  const body = {
+    [`${newMsg[0]}In`]: newMsg[2],
+    [`${newMsg[0]}Out`]: newMsg[3],
   };
-  console.log(updateBody);
-  queries.update(id, updateBody).then(record => {
-    response.writeHead(200, { "Content-Type": "text/xml" });
+  return body;
+}
+
+router.post('/sms', (request, response) => {
+  const twiml = new MessagingResponse();
+  twiml.message('Message received!');
+  const updateBody = formatBody(request.body.Body);
+  const id = request.body.Body.replace(/,/g, '').split(' ')[1];
+  queries.update(id, updateBody).then((record) => {
+    response.writeHead(200, { 'Content-Type': 'text/xml' });
     response.end(twiml.toString());
   });
 });
